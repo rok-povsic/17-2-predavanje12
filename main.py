132b2baa58a510aba035c2e90bd90cafde0120b1
Rok Povšič <rok.povsic@gmail.com>
2017-05-23 15:09:45 +0200

    Cron, GET test (predavanje 9).

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from handlers.base import MainHandler, CookieAlertHandler
 from handlers.topics import TopicAddHandler, TopicShowHandler, TopicDeleteHandler
 from handlers.comments import CommentAddHandler
 from tasks.email_new_comment import EmailNewCommentWorker
+from crons.delete_topics import DeleteTopicsCron
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler, name="main-page"),
@@ -13,5 +14,6 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/topic/show/<topic_id:\d+>', TopicShowHandler),
     webapp2.Route('/topic/show/<topic_id:\d+>/comment/add', CommentAddHandler),
     webapp2.Route('/topic/<topic_id:\d+>/delete', TopicDeleteHandler),
-    webapp2.Route('/task/email-new-comment', EmailNewCommentWorker)
+    webapp2.Route('/task/email-new-comment', EmailNewCommentWorker),
+    webapp2.Route('/cron/delete-topics', DeleteTopicsCron),
 ], debug=True)
